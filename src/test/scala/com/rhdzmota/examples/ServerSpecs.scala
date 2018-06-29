b4d042bd529c6d92264c42a7baf748fa87848f4b
rhdzmota <rohdzmota@gmail.com>
2018-06-28 21:02:23 -0500

    Test versionless path
    Perform tests on the current version of the API.

--- src/test/scala/com/rhdzmota/examples/ServerSpecs.scala
+++ src/test/scala/com/rhdzmota/examples/ServerSpecs.scala
@@ -8,48 +8,48 @@ class ServerSpecs extends WordSpec with Matchers with ScalatestRouteTest {
 
   "The Service" should {
 
-    "return a hello-world json (english) for GET request to /v1.0.0/hello" in {
-      Get("/v1.0.0/hello") ~> Server.route ~> check {
+    "return a hello-world json (english) for GET request to /hello" in {
+      Get("/hello") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Hello)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
       }
     }
 
-    "return a hello-world json (english) for GET request to /v1.0.0/hello/json" in {
-      Get("/v1.0.0/hello/json") ~> Server.route ~> check {
+    "return a hello-world json (english) for GET request to /hello/json" in {
+      Get("/hello/json") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Hello)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
       }
     }
 
-    "return a hello-world json (english) for GET request to /v1.0.0/hello/json/lang" in {
-      Get("/v1.0.0/hello/json/lang") ~> Server.route ~> check {
+    "return a hello-world json (english) for GET request to /hello/json/lang" in {
+      Get("/hello/json/lang") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Hello)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
       }
     }
 
-    "return a hello-world json (english) for GET request to /v1.0.0/hello/json/lang/en" in {
-      Get("/v1.0.0/hello/json/lang/en") ~> Server.route ~> check {
+    "return a hello-world json (english) for GET request to /hello/json/lang/en" in {
+      Get("/hello/json/lang/en") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Hello)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
       }
     }
 
-    "return a hello-world json (spanish) for GET request to /v1.0.0/hello/json/lang/es" in {
-      Get("/v1.0.0/hello/json/lang/es") ~> Server.route ~> check {
+    "return a hello-world json (spanish) for GET request to /hello/json/lang/es" in {
+      Get("/hello/json/lang/es") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Hola)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
       }
     }
 
-    "return a hello-world json (french) for GET request to /v1.0.0/hello/json/lang/fr" in {
-      Get("/v1.0.0/hello/json/lang/fr") ~> Server.route ~> check {
+    "return a hello-world json (french) for GET request to /hello/json/lang/fr" in {
+      Get("/hello/json/lang/fr") ~> Server.route ~> check {
         val jsonString = responseAs[String]
         val expectedResponse = Greeting.fromSalutation(Salutation.Bonjour)
         Greeting.fromJson(jsonString).foreach(_ shouldBe expectedResponse)
